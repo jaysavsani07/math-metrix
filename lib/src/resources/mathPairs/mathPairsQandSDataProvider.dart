466a1f605adaa28d0a6fc5044c2993dec18f785b
Mehul Makwana <mehulmk@nividata.com>
2020-04-16 15:33:21 +0530

    Dynamic data add in Math Pairs

--- lib/src/resources/mathPairs/mathPairsQandSDataProvider.dart
+++ lib/src/resources/mathPairs/mathPairsQandSDataProvider.dart
@@ -1,43 +1,39 @@
 import 'package:mathgame/src/models/mathPairs/MathPairsRootQandS.dart';
+import 'package:mathgame/src/utility/mathUtil.dart';
 
 class MathPairsQandSDataProvider {
-  static getMathPairsDataList() {
+  static getMathPairsDataList(int level) {
     List<MathPairsQandS> list = List();
 
-    list.add(MathPairsQandS(
-        1,
-        [
-          MathPair(1, "2", false, true),
-          MathPair(2, "98", false, true),
-          MathPair(3, "24/3", false, true),
-          MathPair(4, "36+11", false, true),
-          MathPair(5, "97+96", false, true),
-          MathPair(6, "117", false, true),
-          MathPair(5, "193", false, true),
-          MathPair(6, "9*13", false, true),
-          MathPair(4, "47", false, true),
-          MathPair(3, "8", false, true),
-          MathPair(2, "98", false, true),
-          MathPair(1, "1*2", false, true)
-        ],
-        12));
-    list.add(MathPairsQandS(
-        2,
-        [
-          MathPair(1, "2", false, true),
-          MathPair(2, "98", false, true),
-          MathPair(3, "24/3", false, true),
-          MathPair(4, "36+11", false, true),
-          MathPair(5, "97+96", false, true),
-          MathPair(6, "117", false, true),
-          MathPair(5, "193", false, true),
-          MathPair(6, "9*13", false, true),
-          MathPair(4, "47", false, true),
-          MathPair(3, "8", false, true),
-          MathPair(2, "98", false, true),
-          MathPair(1, "1*2", false, true)
-        ],
-        12));
+    int i = 0;
+    int min = 3;
+    min = min < 1 ? 1 : min;
+    int max = 10;
+
+    List<MathPair> x = List();
+    while (x.length < 12) {
+      int x1 = MathUtil.generateRandomAnswer(min, max);
+      String x2 = MathUtil.generateRandomSign();
+      int x3 = MathUtil.generateRandomAnswer(min, max);
+      if (MathUtil.evaluate(x1, x2, x3) > 0 &&
+          !list.contains(MathPair(1, "$x1 $x2 $x3", false, true))) {
+        if ((x2 == "/" && x1 > x3 && x1 % x3 == 0) || x2 != "/") {
+          x.add(MathPair(i, "$x1 $x2 $x3", false, true));
+          x.add(MathPair(i, "${MathUtil.evaluate(x1, x2, x3)}", false, true));
+        }
+        i++;
+      }
+    }
+    x.shuffle();
+    print(x.toString());
+    list.add(MathPairsQandS(2, x, 12));
+
     return list;
+  }
+}
+
+void main() {
+  for (int i = 1; i < 2; i++) {
+    MathPairsQandSDataProvider.getMathPairsDataList(1);
   }
 }
