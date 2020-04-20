fc7f1db4a194748d25b88833b97d9cbb39e3454d
Mehul Makwana <mehulmk@nividata.com>
2020-04-20 11:10:01 +0530

    Mental arithmetic dynamic input handle

--- lib/src/resources/mentalArithmetic/mentalArithmeticQandSDataProvider.dart
+++ lib/src/resources/mentalArithmetic/mentalArithmeticQandSDataProvider.dart
@@ -1,4 +1,5 @@
 import 'package:mathgame/src/models/mentalArithmetic/mentalArithmeticQandS.dart';
+import 'package:mathgame/src/utility/mathUtil.dart';
 
 class MentalArithmeticQandSDataProvider {
   static getMentalArithmeticDataList() {
@@ -8,6 +9,29 @@ class MentalArithmeticQandSDataProvider {
     list.add(MentalArithmeticQandS(2, ["5", "/5", "+1", ""], 2));
     list.add(MentalArithmeticQandS(3, ["11", "-6", "*2", ""], 10));
     list.add(MentalArithmeticQandS(4, ["-5", "-6", "+1", ""], -10));
+
+    int i = 0;
+
+    int min = 3;
+    min = min < 1 ? 1 : min;
+    int max = 10;
+
+    while (i < 5) {
+      int x1 = MathUtil.generateRandomAnswer(min, max);
+      String x2 = MathUtil.generateRandomSign();
+      int x3 = MathUtil.generateRandomAnswer(min, max);
+      String x4 = MathUtil.generateRandomSign();
+      int x5 = MathUtil.generateRandomAnswer(min, max);
+      int x = MathUtil.evaluate(MathUtil.evaluate(x1, x2, x3), x4, x5);
+      if (!list.contains(MentalArithmeticQandS(
+          1, [x1.toString(), "$x2$x3", "$x4$x5", ""], x))) {
+        if ((x2 == "/" && x1 > x3 && x1 % x3 == 0) || x2 != "/") {
+          list.add(MentalArithmeticQandS(
+              1, [x1.toString(), "$x2$x3", "$x4$x5", ""], x));
+        }
+        i++;
+      }
+    }
     return list;
   }
 }
