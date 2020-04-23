a958af3fd13b9a8f858a5bd56f87892e6e9db4ce
jaysavsani07 <jay@nividata.com>
2020-04-23 11:08:11 +0530

    calculator upto 3 or more digits

--- lib/src/provider/calculatorProvider.dart
+++ lib/src/provider/calculatorProvider.dart
@@ -53,7 +53,7 @@ class CalculatorProvider with ChangeNotifier {
   }
 
   Future<void> checkResult(String answer) async {
-    if (_result.length < 2 && !timeOut) {
+    if (_result.length <= _currentState.answer && !timeOut) {
       _result = _result + answer;
       notifyListeners();
       if (int.parse(_result) == _currentState.answer) {
