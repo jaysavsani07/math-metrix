fd224676983ffba6a87f99f1c9c6fcc406fc64ed
mehulmk <mehulm@nividata.com>
2022-03-23 16:12:18 +0530

    Firebase analytics and crashlytics integrated in android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mathgame/src/ui/app/app.dart';
@@ -8,19 +12,37 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
   final sharedPreferences = await SharedPreferences.getInstance();
+  FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   setupServiceLocator(sharedPreferences);
-  runApp(MultiProvider(providers: [
-    // Provider<SharedPreferences>(create: (context) => sharedPreferences),
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(sharedPreferences: sharedPreferences),
+  runApp(
+    MultiProvider(
+      providers: [
+        // Provider<SharedPreferences>(create: (context) => sharedPreferences),
+        ChangeNotifierProvider(
+          create: (context) =>
+              ThemeProvider(sharedPreferences: sharedPreferences),
+        ),
+        ChangeNotifierProvider<DashboardProvider>(
+          create: (context) => GetIt.I.get<DashboardProvider>(),
+        )
+      ],
+      child: MyApp(
+        firebaseAnalytics: firebaseAnalytics,
+      ),
     ),
-    ChangeNotifierProvider<DashboardProvider>(
-      create: (context) => GetIt.I.get<DashboardProvider>(),
-    )
-  ], child: MyApp()));
+  );
 }
 
 setupServiceLocator(SharedPreferences sharedPreferences) {
-  GetIt.I.registerSingleton<DashboardProvider>(DashboardProvider(preferences: sharedPreferences));
+  GetIt.I.registerSingleton<DashboardProvider>(
+      DashboardProvider(preferences: sharedPreferences));
 }
